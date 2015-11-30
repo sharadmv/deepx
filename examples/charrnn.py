95a131a1bc1eb6f25e3892323ef2f8235f2e78c4
William Fedus <liam.fedus@gmail.com>
2015-11-29 22:37:35 -0800

    Discriminative model training

--- examples/charrnn.py
+++ examples/charrnn.py
@@ -138,5 +138,5 @@ if __name__ == "__main__":
     )
 
     rmsprop = RMSProp(discriminator)
-    trainer = Trainer(rmsprop, batcher, callbacks=[])
+    trainer = Trainer(rmsprop, batcher, callbacks=[ ])
     
