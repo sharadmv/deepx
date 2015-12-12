1ec7a5f454a2a960f84503419c016c8229e370eb
Sharad Vikram <sharad.vikram@gmail.com>
2015-12-12 12:27:37 -0500

    fixes to concat

--- deepx/node/node.py
+++ deepx/node/node.py
@@ -2,7 +2,6 @@ import theano
 import theano.tensor as T
 import numpy as np
 
-from exceptions import DimensionException
 from model import Model
 
 class Node(object):
@@ -13,21 +12,22 @@ class Node(object):
 
         self.node_chain = [self]
         self.parameters = {}
-        self._initialized = False
         self.input = None
+
+    @property
+    def _initialized(self):
+        return not (None in self.shape)
 
     # Node operations
 
-    def infer(self, shape_in=None):
-        self.shape_in = self.shape_in or shape_in
-        if self.shape_in is not None:
-            self.shape_out = self._infer(self.shape_in)
-        if None not in self.shape:
-            self.init_parameters()
-            self._initialized = True
+    def infer(self, *inputs):
+        shape_out = [a.shape_out for a in inputs]
+        if None in shape_out:
+            return self.shape_out
+        return self._infer(*shape_out)
 
     def init_parameter(self, name, shape):
-        param = theano.shared(np.random.normal(size=shape) * 0.01)
+        param = theano.shared((np.random.normal(size=shape) * 0.01).astype(theano.config.floatX))
         self.parameters[name] = param
         return param
 
@@ -41,16 +41,29 @@ class Node(object):
         new_node.infer_shape()
         return new_node
 
+    def concatenate(self, node):
+        new_node = ConcatenatedNode(self, node)
+        new_node.infer_shape()
+        return new_node
+
     def create_model(self, mixins):
         if isinstance(mixins, tuple) or isinstance(mixins, list):
             return Model(self, mixins)
         return Model(self, [mixins])
 
     def infer_shape(self):
-        shape_out = self.node_chain[0].shape_in
+        inputs = self.get_input()
+        shape_out =  [a.shape_out for a in inputs]
+        if len(shape_out) == 1:
+            shape_out = shape_out[0]
         for node in self.node_chain:
-            node.infer(shape_out)
-            shape_out = node.shape_out
+            if node.shape_in is None:
+                node.shape_in = shape_out
+            shape_out = node.infer(*inputs)
+            node.shape_out = shape_out
+            inputs = [node]
+            if node._initialized:
+                node.init_parameters()
 
     def get_parameters(self):
         params = []
@@ -66,42 +79,22 @@ class Node(object):
     def __rshift__(self, node):
         return self.chain(node)
 
+    def __add__(self, node):
+        return self.concatenate(node)
+
     def __or__(self, mixins):
         return self.create_model(mixins)
 
     # Getters and setters
 
     def get_input(self):
-        input = self.node_chain[0]
-        if not input.is_data():
-            raise Exception("Cannot have chain without data at the beginning.")
-        return [input]
+        return self.node_chain[0].get_input()
 
     def get_shape_in(self):
-        return self.shape_in
+        return self.node_chain[0].shape_out
 
     def get_shape_out(self):
-        return self.shape_out
-
-    def get_in_nodes(self):
-        return self.in_nodes
-
-    def get_in_node(self):
-        return list(self.in_nodes)[0]
-
-    def get_out_node(self):
-        return self.out_node
-
-    def add_neighbor(self, node):
-        self.neighbors.add(node)
-
-    def get_neighbors(self):
-        return self.neighbors
-
-    def get_neighbor(self):
-        if len(self.neighbors) == 0:
-            return None
-        return list(self.neighbors)[0]
+        return self.node_chain[-1].shape_out
 
     def __iter__(self):
         for node in self.node_chain:
@@ -114,14 +107,15 @@ class Node(object):
         return True
 
     def init_parameters(self):
-        pass
+        # No parameters for default node
+        return
 
     def get_activation(self):
-        input = self.get_input()[0]
-        X = input
-        for node in self.node_chain[1:]:
-            X = node.forward(X)
-        return X
+        X = self.get_input()
+        for node in self.node_chain:
+            X = node.forward(*X)
+            X = [X]
+        return X[0]
 
     def is_data(self):
         return False
@@ -129,6 +123,9 @@ class Node(object):
     @property
     def shape(self):
         return (self.shape_in, self.shape_out)
+
+    def __getitem__(self, idx):
+        return self.node_chain[idx]
 
     def __str__(self):
         return " >> ".join(n.to_str() for n in self)
@@ -145,22 +142,54 @@ class Node(object):
     def _infer(self, shape_in):
         return None
 
-    def forward(self, X):
-        return Data(self._forward(X.get_data()), self.shape_out)
+    def forward(self, *args):
+        return Data(self._forward(*[x.get_data() for x in args]), self.shape_out)
 
     def _forward(self, X):
         raise NotImplementedError
+
+
+class ConcatenatedNode(Node):
+
+    def __init__(self, left_chain, right_chain):
+        super(ConcatenatedNode, self).__init__()
+        self.left_chain, self.right_chain = left_chain, right_chain
+
+    def _infer(self, shape_left, shape_right):
+        return self.left_chain.get_shape_out() + self.right_chain.get_shape_out()
+
+    def forward(self, X, Y):
+        X = self.left_chain.get_activation()
+        Y = self.right_chain.get_activation()
+        return X.concat(Y)
+
+    def get_input(self):
+        return self.left_chain.get_input() + self.right_chain.get_input()
 
 class Data(Node):
 
     def __init__(self, data, shape):
         super(Data, self).__init__()
         self.data = data
+        self.shape_in = shape
         self.shape_out = shape
+
+    def _infer(self, args):
+        return args
+
+    def forward(self, X):
+        return X
 
     @property
     def ndim(self):
         return self.data.ndim
+
+    def concat(self, data):
+        my_data, other_data = self.get_data(), data.get_data()
+        return Data(T.concatenate([my_data, other_data], axis=-1), self.shape_out + data.shape_out)
+
+    def get_input(self):
+        return [self]
 
     def get_data(self):
         return self.data
