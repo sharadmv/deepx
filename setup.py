b70c9b7d9d2d2a4399f308e7338cba3a3d87f055
Sharad Vikram <sharad.vikram@gmail.com>
2015-12-18 18:47:37 -0800

    updated version and README

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "deepx",
-    version = "0.0.4",
+    version = "0.1.0",
     author = "Sharad Vikram and Zachary Chase Lipton",
     author_email = "sharad.vikram@gmail.com",
     description = "A basic deep learning library for sequence learning built using Theano.",
