76ce09232db0187fe77098195513ce980d36c6b8
Sharad Vikram <sharad.vikram@gmail.com>
2016-01-03 21:19:12 -0800

    fixed tensorflow tests

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -2,10 +2,11 @@ from base import BaseTest
 
 import numpy as np
 from deepx.nn import Vector, Lambda
+import deepx.backend as T
 
 class TestUtility(BaseTest):
 
-    def run_lambda(self, func, shape_func=lambda x: x):
+    def run_lambda(self, func, np_func, shape_func=lambda x: x):
         np.random.seed(0)
 
         in_size = 10
@@ -20,12 +21,11 @@ class TestUtility(BaseTest):
         X = np.random.normal(size=(B, in_size))
 
         result = net_func(X)
-        result2 = func(X)
+        result2 = np_func(X)
 
         np.testing.assert_almost_equal(result, result2, decimal=5)
 
     def test_lambda1(self):
-        self.run_lambda(lambda x: x)
-        self.run_lambda(lambda x: x * 2)
-        self.run_lambda(lambda x: x ** 2)
-        self.run_lambda(lambda x: abs(x))
+        self.run_lambda(lambda x: x * 2, lambda x: x * 2)
+        self.run_lambda(lambda x: T.pow(x, 2), lambda x: x ** 2)
+        self.run_lambda(lambda x: abs(x), lambda x: abs(x))
