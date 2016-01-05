56d7cf5de196c2c2486d495c71bc1c2a9a247698
Sharad Vikram <sharad.vikram@gmail.com>
2016-01-04 21:32:13 -0800

    fixed python3 compat bug

--- deepx/nn/ops.py
+++ deepx/nn/ops.py
@@ -1,6 +1,6 @@
 def Repeat(node,  n_repeats):
     new_node = node.copy()
-    for i in xrange(n_repeats - 1):
+    for i in range(n_repeats - 1):
         new_node = new_node.chain(node.copy())
     return new_node
 
