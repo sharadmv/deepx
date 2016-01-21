dac35b8f8c94ac5447720235bdb9015a7c1e1ad9
Sharad Vikram <sharad.vikram@gmail.com>
2016-01-21 02:16:01 -0800

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "deepx",
-    version = "0.1.11",
+    version = "0.1.12",
     author = "Sharad Vikram and Zachary Chase Lipton",
     author_email = "sharad.vikram@gmail.com",
     description = "A basic deep learning library.",
