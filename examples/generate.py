9f62a201a0d2d0a5333a671058531e1177fa32bc
Sharad Vikram <sharad.vikram@gmail.com>
2016-02-12 15:45:53 -0800

    Update generate.py

--- examples/generate.py
+++ examples/generate.py
@@ -6,9 +6,4 @@ from deepx.loss import *
 from deepx.optimize import *
 
 if __name__ == "__main__":
-    lstm = Vector(100) >> Repeat(LSTM(1024), 2) >> Softmax(100)
-    gen = Generate(lstm, 100)
-
-    gen2 = Sequence(Vector(100, 1)) >> Repeat(LSTM(1024, stateful=True), 2) >> Softmax(100)
-
-    # wat = RMSProp(gen, CrossEntropy())
+    gen = Generate(Vector(100) >> Repeat(LSTM(1024), 2) >> Softmax(100), 100)
