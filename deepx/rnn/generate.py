0d481bf020525885bf2b71b6f4db99e21822b441
Sharad Vikram <sharad.vikram@gmail.com>
2016-03-23 18:12:15 -0700

    updates to generate

--- deepx/rnn/generate.py
+++ deepx/rnn/generate.py
@@ -1,32 +1,54 @@
+import numpy as np
 from .. import backend as T
 from ..node import Node
 from ..util import pack_tuple, unpack_tuple
 
 class Generate(Node):
 
-    def __init__(self, node, length=None):
+    def __init__(self, node, length=None, sharpen=np.array(0).astype(np.float32)):
         super(Generate, self).__init__()
         self.node = node
         self.length = length
+        self._sample = None
+        self.updates = None
+        self.sharpen_amount = sharpen
         assert self.node.get_shape_out() == self.node.get_shape_in()
 
     def forward(self, X, **kwargs):
+        output = self.generate(X, **kwargs)
+        out = X.next(output[1], self.get_shape_out())
+        out.sequence = True
+        out.sequence_length = self.length
+        return out
+
+    def sample(self, X):
+        if self._sample is None:
+            input = self.get_input()
+            output = self.generate(input, use_dropout=False)[0]
+            self._sample = T.function(self.get_formatted_input(), [output], updates=self.get_updates())
+        return self._sample(X)
+
+    def sharpen(self, x, idx):
+        sharpened = x + idx * self.sharpen_amount
+        return sharpened / T.sum(sharpened, axis=1)[:, None]
+
+    def generate(self, X, **kwargs):
         states = self.node.get_initial_states(X.get_data(), shape_index=0)
         states, shape = unpack_tuple(states)
 
-        def step(input, *states):
+        def step(input, _, *states):
             packed_state = pack_tuple(states, shape)
             output_softmax, next_state = self.node.step(X.next(input), packed_state)
             output_softmax = output_softmax.get_data()
             output_sample = T.sample(output_softmax)
+            output_softmax = self.sharpen(output_softmax, output_sample)
             states, _ = unpack_tuple(next_state)
-            return [output_sample] + list(states)
+            return [output_sample, output_softmax] + list(states)
 
-        output, self.updates = T.generate(step, [X.get_data()] + list(states), self.length)
-        out = X.next(output[0], self.get_shape_out())
-        out.sequence = True
-        out.sequence_length = self.length
-        return out
+        output, updates = T.generate(step, [X.get_data(), X.get_data()] + list(states), self.length)
+        if self.updates is None:
+            self.updates = updates
+        return output
 
     def can_initialize(self):
         return True
