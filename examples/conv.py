df424aae9afe73ce9f262659e2876e724f0bb948
Sharad Vikram <sharad.vikram@gmail.com>
2017-09-23 14:58:33 -0700

    updated version

--- examples/conv.py
+++ examples/conv.py
@@ -1,45 +1,54 @@
 from __future__ import print_function
 
+import tensorflow as tf
 import numpy as np
 from sklearn.datasets import fetch_mldata
 
 from deepx.nn import *
-from deepx.loss import *
-from deepx.optimize import *
+from deepx import T
 
 if __name__ == "__main__":
-    # mnist = fetch_mldata("MNIST original")
+    mnist = fetch_mldata("MNIST original")
 
-    # X = mnist['data']
-    # N = X.shape[0]
-    # X = X.reshape((N, 1, 28, 28))
-    # labels = mnist['target']
+    X = mnist['data']
+    N = X.shape[0]
+    X = X.reshape((N, 28, 28, 1))
+    labels = mnist['target']
 
-    # np.random.seed(0)
-    # idx = np.random.permutation(np.arange(70000))
-    # X = X[idx]
-    # labels = labels[idx].astype(np.int32)
+    np.random.seed(0)
+    idx = np.random.permutation(np.arange(70000))
+    X = X[idx]
+    labels = labels[idx].astype(np.int32)
 
-    # y = np.zeros((N, 10))
-    # for i in range(N):
-        # y[i, labels[i]] = 1
+    y = np.zeros((N, 10))
+    for i in range(N):
+        y[i, labels[i]] = 1
 
-    # split = int(0.9 * N)
+    split = int(0.9 * N)
 
-    # train_idx, test_idx = idx[:split], idx[split:]
+    train_idx, test_idx = idx[:split], idx[split:]
 
-    # Xtrain, Xtest = X[train_idx], X[test_idx]
-    # ytrain, ytest = y[train_idx], y[test_idx]
+    Xtrain, Xtest = X[train_idx], X[test_idx]
+    ytrain, ytest = y[train_idx], y[test_idx]
 
-    conv_net = Image((1, 28, 28)) >> Conv((10, 2, 2), activation=Elu) >> Conv((20, 2, 2)) >> Flatten() >> Softmax(10)
+    X_in = T.placeholder(T.floatx(), [None, 28, 28, 1])
+    Y_in = T.placeholder(T.floatx(), [None, 10])
 
-    rmsprop = RMSProp(conv_net >> CrossEntropy())
+    conv_net = Conv((2, 2, 10)) >> Conv((2, 2, 20)) >> Flatten() >> Linear(10)
+    logits = conv_net(X_in)
+    predictions = T.argmax(logits, -1)
+    loss = T.mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y_in))
 
-    # def train(n_iter, lr):
-        # for i in range(n_iter):
-            # u = np.random.choice(np.arange(split))
-            # loss = rmsprop.train(Xtrain[u:u+50], ytrain[u:u+50], lr)
-            # print("Loss:", loss)
+    train_op = tf.train.AdamOptimizer(1e-3).minimize(loss)
 
-        # preds = conv_net(Xtest).argmax(axis=1)
-        # print("Error: ", 1 - (preds == labels[test_idx]).sum() / float(N - split))
+    sess = T.interactive_session()
+
+    def train(n_iter, batch_size=20):
+        for i in range(n_iter):
+            idx = np.random.permutation(Xtrain.shape[0])[:batch_size]
+            result = sess.run([loss, train_op], { X_in : Xtrain[idx], Y_in : ytrain[idx] })
+            print("Loss:", result[0])
+
+        preds = sess.run(predictions, { X_in : Xtest }).astype(np.int32)
+        print("Error: ", 1 - (preds == labels[test_idx]).sum() / float(N - split))
+    train(1000)
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ recommended = {
 
 setup(
     name = "deepx",
-    version = "0.3.9",
+    version = "0.3.10",
     author = "Sharad Vikram",
     author_email = "sharad.vikram@gmail.com",
     description = "A basic deep learning library.",
