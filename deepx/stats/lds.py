fd23102dffbcc4d38235478f7bf17f9c15c0fcc2
Marvin <marvin@eecs.berkeley.edu>
2018-12-13 17:38:53 -0800

    working implementation of LDS

--- deepx/stats/lds.py
+++ deepx/stats/lds.py
@@ -1,5 +1,6 @@
 import numpy as np
 from .. import T
+import tensorflow as tf
 
 from .common import ExponentialFamily
 from .gaussian import Gaussian
@@ -35,8 +36,7 @@ class LDS(ExponentialFamily):
     def log_z(self):
         if not 'log_z' in self._cached:
             natparam = self.get_parameters('natural')
-            N, ds = T.shape(natparam)[0], (T.shape(natparam)[2] - 1) // 2
-            H = self._parameter_cache['internal'][0][0].get_shape()[0] + 1
+            N, H, ds = T.shape(natparam)[0], natparam.get_shape()[1], (T.shape(natparam)[2] - 1) // 2
             pred_potential = T.zeros((N, ds+1, ds+1))
 
             for t_ in range(H):
@@ -67,75 +67,64 @@ class LDS(ExponentialFamily):
         ds = (T.shape(ess)[-1] - 1) // 2
         return ess[..., :ds, -1]
 
-    def filter(self, smooth=False):
-        key = 'smoothed' if smooth else 'filtered'
-        if not key in self._cached:
-            parameters = self.get_parameters('internal')
-            assert len(parameters) == 4, 'missing state node potentials?'
-            (A, B, Q), prior, potentials, actions = parameters
-            Q_inv = T.matrix_inverse(Q)
-            A, B, Q, Q_inv = tuple(map(
-                lambda x: T.core.pad(x, [[0, 1], [0, 0], [0, 0]]),
-                (A, B, Q, Q_inv)
-            ))
-            N, H, ds = T.shape(potentials[1])[0], T.shape(potentials[1])[1], T.shape(potentials[1])[2]
+    def smooth(self):
+        ess = self.expected_sufficient_statistics()
+        ds = (T.shape(ess)[-1] - 1) // 2
+        mu = ess[..., :ds, -1]
+        sig = ess[..., :ds, :ds] - T.outer(mu, mu)
+        return Gaussian([sig, mu])
 
-            J_22 = Q_inv
-            J_12 = -T.einsum('tba,tbc->tac', A, J_22)
-            J_21 = T.matrix_transpose(J_12)
-            J_11 = -T.einsum('tab,tbc->tac', J_12, A)
-            h2 = T.einsum('ita,tba,tbc->tic', actions, B, J_22)
-            h1 = -T.einsum('tia,tab->tib', h2, A)
-            self._cached['info_params'] = (J_11, J_12, J_22, h1, h2)
+    def filter(self):
+        parameters = self.get_parameters('internal')
+        assert len(parameters) == 4, 'missing state node potentials?'
+        (A, B, Q), prior, potentials, actions = parameters
+        prior_natparams = Gaussian.unpack(prior.get_parameters('natural'))
+        A_inv, Q_inv = T.matrix_inverse(A), T.matrix_inverse(Q)
+        A, B, Q, Q_inv = tuple(map(
+            lambda x: T.core.pad(x, [[0, 1], [0, 0], [0, 0]]),
+            (A, B, Q, Q_inv)
+        ))
+        N, H, ds = T.shape(potentials[1])[0], T.shape(potentials[1])[1], T.shape(potentials[1])[2]
+        A_inv = T.concat([A_inv, T.eye(ds)[None]], axis=0)
 
-            potentials = (-2 * T.transpose(potentials[0], [1, 0, 2, 3]), T.transpose(potentials[1], [1, 0, 2]))
+        J_22 = Q_inv
+        J_12 = -T.einsum('tba,tbc->tac', A, J_22)
+        J_21 = T.matrix_transpose(J_12)
+        J_11 = -T.einsum('tab,tbc->tac', J_12, A)
+        h2 = T.einsum('ita,tba,tbc->tic', actions, B, J_22)
+        h1 = -T.einsum('tia,tab->tib', h2, A)
+        self._cached['info_params'] = (J_11, J_12, J_22, h1, h2)
 
-            def kalman_filter(previous, potential):
-                t_, _, prev = previous
-                J_tt = prev[0] + potential[0]
-                h_tt = prev[1] + potential[1]
-                mat_inv = T.einsum('ab,ibc->iac', J_21[t_], T.matrix_inverse(J_tt + J_11[t_][None]))
-                J_t1_t = J_22[t_][None] - T.einsum('iab,bc->iac', mat_inv, J_12[t_])
-                h_t1_t = h2[t_] - T.einsum('iab,ib->ib', mat_inv, h_tt + h1[t_])
+        potentials = (-2 * T.transpose(potentials[0], [1, 0, 2, 3]), T.transpose(potentials[1], [1, 0, 2]))
+        actions = T.transpose(actions, [1, 0, 2])
+        prior_natparams[0] = T.tile(prior_natparams[0][None], [N, 1, 1])
+        prior_natparams[1] = T.tile(prior_natparams[1][None], [N, 1])
 
-                return t_ + 1, (J_tt, h_tt), (J_t1_t, h_t1_t)
+        def kalman_filter(previous, potential):
+            t_, _, prev = previous
+            J_tt = prev[0] + potential[0]
+            h_tt = prev[1] + potential[1]
+            M = T.einsum('ba,ibc,cd->iad', A_inv[t_], J_tt, A_inv[t_])
+            inv_term = T.eye(ds) + T.einsum('iab,bc->iac', M, Q[t_])
+            J_t1_t = T.matrix_solve(inv_term, M)
+            h_t1_t = T.einsum('iab,ib->ia',
+                              T.matrix_solve(inv_term, T.tile(T.transpose(A_inv[t_])[None], [N, 1, 1])),
+                              h_tt) + \
+                    T.einsum('iab,bc,ic->ia', J_t1_t, B[t_], actions[t_])
 
-            _, filtered, _ = T.scan(kalman_filter, potentials,
-                                    (0,
-                                     (T.eye(ds, batch_shape=[N]) * 1e-4, T.zeros([N, ds])),
-                                     (T.eye(ds, batch_shape=[N]) * 1e-4, T.zeros([N, ds])))
-            )
-            filtered = (T.transpose(filtered[0], [1, 0, 2, 3]), T.transpose(filtered[1], [1, 0, 2]))
+            return t_ + 1, (J_tt, h_tt), (J_t1_t, h_t1_t)
 
-            if smooth:
-                def kalman_smooth(previous, potential):
-                    t_, prev = previous
-
-                    mat_inv = T.einsum('ab,ibc->iac', J_21[t_], T.matrix_inverse(potential[0] + J_11[t_][None]))
-                    J_t1_t = J_22[t_][None] - T.einsum('iab,bc->iac', mat_inv, J_12[t_])
-                    h_t1_t = h2[t_] - T.einsum('iab,ib->ib', mat_inv, potential[1] + h1[t_])
-
-                    mat_inv = T.einsum('ab,ibc->iac', J_12[t_], T.matrix_inverse(prev[0] - J_t1_t + J_22[t_][None]))
-                    J_tT = potential[0] + J_11[t_][None] - T.einsum('ab,ibc,dc->iad', J_12[t_], mat_inv, J_12[t_])
-                    h_tT = potential[1] + h1[t_] - T.einsum('ab,ibc,ic->ia', J_12[t_], mat_inv, prev[1] - h_t1_t + h2[t_])
-                    return t_ + 1, (J_tT, h_tT)
-                _, smoothed = T.core.scan(kalman_smooth,
-                                          (filtered[0][:-1], filtered[1][:-1]),
-                                          (0, (filtered[0][-1], filtered[1][-1])),
-                                          reverse=True)
-                smoothed = (
-                    T.concatenate([smoothed[0], filtered[0][-1:]], 0),
-                    T.concatenate([smoothed[1], filtered[1][-1:]], 0),
-                )
-            else:
-                smoothed = filtered
-            self._cached[key] = smoothed
-        else:
-            smoothed = self._cached[key]
+        _, filtered, _ = T.scan(kalman_filter, potentials,
+                                (0,
+                                 (T.zeros([N, ds, ds]), T.zeros([N, ds])),
+                                 (-2 * prior_natparams[0], prior_natparams[1]))
+        )
+        filtered = (T.transpose(filtered[0], [1, 0, 2, 3]), T.transpose(filtered[1], [1, 0, 2]))
+        self._cached['filtered'] = filtered
 
         return Gaussian([
-            T.matrix_inverse(smoothed[0]),
-            T.matrix_solve(smoothed[0], smoothed[1][..., None])[..., 0]
+            T.matrix_inverse(filtered[0]),
+            T.matrix_solve(filtered[0], filtered[1][..., None])[..., 0]
         ])
 
     def sample(self, num_samples=1):
@@ -190,9 +179,9 @@ class LDS(ExponentialFamily):
         prior_eta1, prior_eta2 = Gaussian.unpack(prior.get_parameters('natural'))
         prior_natparam = T.core.pad(
             vs([
-                hs([prior_eta1, T.zeros([ds, ds]), prior_eta2[..., None]]),
+                hs([prior_eta1, T.zeros([ds, ds]), 0.5 * prior_eta2[..., None]]),
                 T.zeros([ds, 2 * ds + 1]),
-                hs([prior_eta2[None], T.zeros([1, ds]), T.to_float(-prior.log_z())[None, None]]),
+                0.5 * hs([prior_eta2[None], T.zeros([1, ds]), T.to_float(-prior.log_z())[None, None]]),
             ])[None],
             [[0, H - 1], [0, 0], [0, 0]]
         )
@@ -215,9 +204,9 @@ class LDS(ExponentialFamily):
             N = T.shape(potentials[0])[0]
             potentials_natparam = (
                 vs([
-                  hs([potentials[0], T.zeros([N, H, ds, ds]), potentials[1][..., None]]),
+                  hs([potentials[0], T.zeros([N, H, ds, ds]), 0.5 * potentials[1][..., None]]),
                   T.zeros([N, H, ds, 2 * ds + 1]),
-                  hs([potentials[1][..., None, :], T.zeros([N, H, 1, ds]), T.to_float(-potentials[2])[..., None, None]]),
+                  0.5 * hs([potentials[1][..., None, :], T.zeros([N, H, 1, ds]), T.to_float(-potentials[2])[..., None, None]]),
                 ])
             )
             return prior_natparam + dynamics_natparam + potentials_natparam
