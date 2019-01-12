54442108e88ef5564d5721bd39aec3fadfa9f885
Sharad Vikram <sharad.vikram@gmail.com>
2019-01-11 18:15:11 -0800

    slightly working batch norm

--- deepx/nn/batch_norm.py
+++ deepx/nn/batch_norm.py
@@ -1,3 +1,4 @@
+import numpy as np
 from .. import T
 from ..layer import Layer
 
@@ -7,12 +8,27 @@ class BatchNorm(Layer):
         super(BatchNorm, self).__init__(**kwargs)
 
     def initialize(self):
-        self.create_parameter('beta', self.get_dim_in(), value=0)
-        self.create_parameter('gamma', self.get_dim_in(), value=1)
+        self.create_parameter('beta', self.get_dim_in(), initial_value=np.zeros(self.get_dim_in()))
+        self.create_parameter('gamma', self.get_dim_in(), initial_value=np.ones(self.get_dim_out()))
 
-    def infer(self, shape_in):
-        return shape_in
+    def infer_shape(self, shape):
+        if shape is None: return
+        self.dim_in = shape
+        self.dim_out = shape
+
+    def get_dim_in(self):
+        return self.dim_in
+
+    def get_dim_out(self):
+        return self.dim_out
+
+    def is_initialized(self):
+        return not (self.dim_in is None or self.dim_out is None)
 
     def forward(self, X):
         beta, gamma = self.get_parameter('beta'), self.get_parameter('gamma')
         return T.batch_norm(X, beta, gamma)
+
+    def __str__(self):
+        name = self.__class__.__name__
+        return "%s()" % (name)
