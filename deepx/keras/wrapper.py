8a3e8750cd5b914673cf1301cabb8d3b3308bbe7
Sharad Vikram <sharad.vikram@gmail.com>
2019-04-03 17:25:55 -0700

    figuring out keras shapes

--- deepx/keras/wrapper.py
+++ deepx/keras/wrapper.py
@@ -1,3 +1,4 @@
+from deepx.backend import T
 from deepx.core import Layer
 
 class KerasLayer(Layer):
@@ -7,26 +8,16 @@ class KerasLayer(Layer):
         self.layer = layer
 
     def shape_inference(self):
-        pass
+        if self.get_shape_in() is not None:
+            placeholder = T.ones(self.get_shape_in()[0])
+            output = self.layer(placeholder)
+            self.set_shape_out([T.get_shape(output)])
 
     def is_initialized(self):
         return True
 
     def get_parameters(self):
         return self.layer.trainable_weights
-
-    def get_shape_out(self):
-        try:
-            return self.layer.output_shape
-        except:
-            return None
-
-    def get_shape_in(self):
-        try:
-            return self.layer.input_shape
-        except:
-            return None
-
 
     def _forward(self, X):
         return self.layer(X)
